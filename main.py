a05d7294fcaf78e98b5ce652b04e1b0e11f1aa2a
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-06-27 22:20:50 +0200

    Initial commit

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import re
 from bs4 import BeautifulSoup
 import requests
 
-response = requests.get(
-    "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+url = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(url)
 movies_webpage = response.text
 
 soup = BeautifulSoup(movies_webpage, "html.parser")
-movie_titles = [movie.getText() for movie in soup.find_all(name="h3", class_="title")]
+
+movie_titles = [movie.getText() for movie in soup.find_all("h3", class_="title")]
+
 sorted_movie_titles = sorted(movie_titles, key=lambda x: int(re.search(r"\d+", x).group()))
+
 print(sorted_movie_titles)
 
 with open("movie_list.txt", "w") as file:
-    for movie in sorted_movie_titles:
-        file.write(f"{movie}\n")
+    file.write("\n".join(sorted_movie_titles))
